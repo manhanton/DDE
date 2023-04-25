42f8c5b81d8454b3f683ad65a457103fcfba5025
Trin Puko K. Nin <30490520+manhanton@users.noreply.github.com>
2023-04-25 21:10:17 +0700

    Change to readable script

--- dev_name_thai.py
+++ dev_name_thai.py
@@ -1,11 +1,8 @@
 import csv
+import requests
+from io import StringIO
 import string
 from math import sqrt
-
-# Read the sentences from a CSV file
-with open('dev_name.csv', 'r',encoding='cp1252') as file:
-    reader = csv.reader(file)
-    sentences = [row[0] for row in reader]
 
 # Tokenize and preprocess the sentences
 def preprocess(sentence):
@@ -14,15 +11,8 @@ def preprocess(sentence):
     tokens = sentence.split()
     return tokens
 
-tokenized_sentences = [preprocess(sentence) for sentence in sentences]
-
-# Create a set of all unique words in all sentences
-unique_words = set()
-for tokens in tokenized_sentences:
-    unique_words.update(tokens)
-
 # Create a bag-of-words vector for each sentence
-def create_vector(tokens):
+def create_vector(tokens, unique_words):
     vector = []
     for word in unique_words:
         if word in tokens:
@@ -31,9 +21,7 @@ def create_vector(tokens):
             vector.append(0)
     return vector
 
-vectors = [create_vector(tokens) for tokens in tokenized_sentences]
-
-# Calculate the cosine similarity between each pair of sentences
+# Calculate the cosine similarity between two vectors
 def cosine_similarity(v1, v2):
     dot_product = sum([v1[i] * v2[i] for i in range(len(v1))])
     magnitude_v1 = sqrt(sum([x**2 for x in v1]))
@@ -43,14 +31,43 @@ def cosine_similarity(v1, v2):
     else:
         return dot_product / (magnitude_v1 * magnitude_v2)
 
+# Read the developer names from a CSV file
+def read_developer_names(file_url):
+    response = requests.get(file_url)
+    csv_buffer = StringIO(response.content.decode('utf-8'))
+    csv_reader = csv.reader(csv_buffer, delimiter=',')
+    # Skip the header row
+    next(csv_reader)
+    # Extract the developer names from the CSV file
+    developer_names = [row[0] for row in csv_reader]
+    return developer_names
 
-# Calculate the cosine similarity between each pair of sentences
-similarities = []
-for i in range(len(sentences)):
-    for j in range(i+1, len(sentences)):
-        similarity = cosine_similarity(vectors[i], vectors[j])
-        similarities.append((i, j, similarity))
+# Calculate the similarity between each pair of developer names
+def calculate_similarity(developer_names):
+    # Tokenize and preprocess the developer names
+    tokenized_names = [preprocess(name) for name in developer_names]
+    # Create a set of all unique words in all names
+    unique_words = set()
+    for tokens in tokenized_names:
+        unique_words.update(tokens)
+    # Create a bag-of-words vector for each name
+    vectors = [create_vector(tokens, unique_words) for tokens in tokenized_names]
+    # Calculate the cosine similarity between each pair of names
+    similarities = []
+    for i in range(len(developer_names)):
+        for j in range(i+1, len(developer_names)):
+            similarity = cosine_similarity(vectors[i], vectors[j])
+            similarities.append((i, j, similarity))
+    return similarities
 
-# Print the similarity scores
-for i, j, similarity in similarities:
-    print(f"Similarity between sentence {i+1} and sentence {j+1}: {similarity:.2f}")
+# Main function
+def main():
+    file_url = "https://raw.githubusercontent.com/manhanton/DDE/main/dev_name.csv"
+    developer_names = read_developer_names(file_url)
+    similarities = calculate_similarity(developer_names)
+    # Print the similarity scores
+    for i, j, similarity in similarities:
+        print(f"Similarity between developer {i+1} and developer {j+1}: {similarity:.2f}")
+
+if __name__ == "__main__":
+    main()
